7b3a28ad32dc44904b89ebd81161ad6bb8b3d67a
axel <95295133+axou12202@users.noreply.github.com>
2026-01-18 18:40:53 +0100

    fix: remove remaining src imports

--- backend/src/sync/sync.module.ts
+++ backend/src/sync/sync.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from '../prisma/prisma.module';
-import { TcgdexModule } from 'src/tcgdex/tcgdex.module';
+import { TcgdexModule } from '../tcgdex/tcgdex.module';
 import { SyncService } from './sync.service';
 import { SyncController } from './sync.controller';
 
